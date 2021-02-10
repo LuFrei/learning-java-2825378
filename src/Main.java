58989d285c00288d8c18778b387355e724176a44
LuFrei <lucasfreitas09876@gmail.com>
2021-02-10 09:54:49 -0500

    string indexes

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,18 @@ public class Main {
     public static void main(String[] args) {
         int studentAge = 15;
         double studentGPA = 2.86;
-        char studentFirst = 'L';
-        char studentLast = 'F';
         boolean hasPerfectAttendance = true;
+        String studentFirstName = "Lucas";
+        String studentLastName = "Freitas";
+        char studentFirstInitial = studentFirstName.charAt(0);
+        char studentLastInitial = studentLastName.charAt(0);
 
         System.out.println(studentAge);
         System.out.println(studentGPA);
-        System.out.println(studentFirst);
-        System.out.println(studentLast);
+        System.out.println(studentFirstInitial);
+        System.out.println(studentLastInitial);
         System.out.println(hasPerfectAttendance);
+        System.out.println(studentFirstName);
 
     }
 }
