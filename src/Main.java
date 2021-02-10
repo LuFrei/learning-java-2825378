4ff4f8741b03328aedc476354fa9a5c8c6608629
LuFrei <lucasfreitas09876@gmail.com>
2021-02-09 19:00:28 -0500

    My first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int studentAge = 15;
+        double studentGPA = 2.86;
+        char studentFirst = 'L';
+        char studentLast = 'F';
+        boolean hasPerfectAttendance = true;
+
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(studentFirst);
+        System.out.println(studentLast);
+        System.out.println(hasPerfectAttendance);
+
+    }
+}
